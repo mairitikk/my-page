00ff9c37b3679ea02051a2b439c21f97c7d7b2fc
mairi tikk <mairi.tikk@gmail.com>
2025-06-04 23:01:34 +0200

    add new logo

--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Navbar.css'
+import drimt from '../../assets/DriMt.svg'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <h1>Tereee Lorem ipsum dolor sit amet consectetur adipisicing elit. A, at asperiores, temporibus ipsam neque accusamus dolore, deleniti sint sit culpa laboriosam ipsa saepe ducimus ut possimus suscipit molestias sequi qui?</h1>
+            <img src={drimt} alt="DriMt Logo" className="navbar-logo" />
         </div>
     )
 }
