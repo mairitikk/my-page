d7e3613ce6651ca328607ed83abaf50c2e8cc435
mairi tikk <mairi.tikk@gmail.com>
2025-06-04 22:07:20 +0200

    color changes

--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-
+        <div className='navbar'>
+            <h1>Tereee Lorem ipsum dolor sit amet consectetur adipisicing elit. A, at asperiores, temporibus ipsam neque accusamus dolore, deleniti sint sit culpa laboriosam ipsa saepe ducimus ut possimus suscipit molestias sequi qui?</h1>
         </div>
     )
 }
